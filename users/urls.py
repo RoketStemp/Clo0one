1ff0c4d7feb76aea4c9287e2c7c96953f24aea0d
RoketStemp <stemproket@gmail.com>
2021-08-04 20:28:13 +0300

    Added login logic
    now user can log in and log out from site

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('login/', views.user_login_view, name='user_login_view'),
+    path('logout/', views.user_logout_view, name='user_logout_view'),
+    path('profile/', views.user_home_page_view, name='user_home_page_view')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, HttpResponse
 
 from .forms import LoginForm
 
 
-def login(request):
-    form = LoginForm()
+def user_login_view(request):
+    """View which allows user to log in on site"""
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('user_home_page_view')
+        else:
+            form.add_error('username', 'Username or password is incorrect')
+    else:
+        form = LoginForm()
+
     return render(request, 'users/login.html', {'form': form})
+
+
+def user_home_page_view(request):
+    """User for showing current user data got by slug"""
+    return render(request, 'users/profile.html')
+
+
+def user_logout_view(request):
+    """User logout view"""
+    logout(request)
+    return redirect('user_login_view')
