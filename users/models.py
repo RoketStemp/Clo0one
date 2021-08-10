e9b3047997e075236044cedb4aee638b1e231487
RoketStemp <stemproket@gmail.com>
2021-08-10 20:31:04 +0300

    Added subscribers and subscriptions to the user profile

--- users/models.py
+++ users/models.py
@@ -19,10 +19,10 @@ class UserSubscriptions(models.Model):
 class CustomUser(models.Model):
     """Extended django User model with new fields profile_photo and description"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = models.ImageField(verbose_name='User profile photo', upload_to='images')
-    description = models.TextField(verbose_name='User info')
-    subscribers = models.ManyToManyField(UserSubscribers)
-    subscriptions = models.ManyToManyField(UserSubscriptions)
+    profile_photo = models.ImageField(verbose_name='User profile photo', upload_to='images', blank=True)
+    description = models.TextField(verbose_name='User info', blank=True)
+    subscribers = models.ManyToManyField(UserSubscribers, blank=True)
+    subscriptions = models.ManyToManyField(UserSubscriptions, blank=True)
 
     def __str__(self):
         return self.user.username
