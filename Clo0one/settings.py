88271f3b3eb69b26fc85df553d5d8dbe55a9c8cf
roketstemp <stemproket@gmail.com>
2021-08-25 16:09:45 +0300

    Db connection fix

--- Clo0one/settings.py
+++ Clo0one/settings.py
@@ -81,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'CloneDB',
-        'USER': 'roketstemp',
-        'PASSWORD': '123',
+        'USER': 'postgres',
+        'PASSWORD': 'dayn123123',
         'HOST': '127.0.0.1',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
